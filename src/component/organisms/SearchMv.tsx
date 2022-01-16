ecae42c982f2aef0bd8e9d0613f4c332eb6ddc0d
Megumu Niihara <nhrmgm1223@gmail.com>
2022-01-16 19:22:52 +0900

    add--検索系のcomponent作成

---
+++ src/component/organisms/SearchMv.tsx
@@ -0,0 +1,63 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { useState, ChangeEvent, VFC, memo, useEffect } from "react";
+
+import { filterOpt } from "../../config/search/settings";
+import { useSearchFilter } from "../../hooks/useSearchFilter";
+import { SelectWithLabel } from "../molecules/input/SelectWithLabel";
+
+export const SearchMv: VFC = memo(() => {
+  const { searchFilter, setSearchFilter } = useSearchFilter();
+  const [selectLanguage, setSelectLanguage] = useState(searchFilter.language);
+  const [selectMedia, setSelectMedia] = useState(searchFilter.media);
+  const [selectTerms, setSelectTerms] = useState(searchFilter.terms);
+
+  useEffect(() => {
+    setSearchFilter({
+      language: selectLanguage,
+      media: selectMedia,
+      terms: selectTerms,
+    });
+  }, [selectLanguage, selectMedia, selectTerms]);
+
+  const onChangeSelectLanguage = (e: ChangeEvent<HTMLSelectElement>) =>
+    setSelectLanguage(e.target.value);
+  const onChangeSelectMedia = (e: ChangeEvent<HTMLSelectElement>) =>
+    setSelectMedia(e.target.value);
+  const onChangeSelectTerms = (e: ChangeEvent<HTMLSelectElement>) =>
+    setSelectTerms(e.target.value);
+
+  return (
+    <Flex
+      gap={{ base: 6, md: 8, lg: 12 }}
+      justify="center"
+      align="center"
+      flexDirection={{ base: "column", sm: "row" }}
+      mb={10}
+    >
+      <Box>
+        <SelectWithLabel
+          labelText="Language"
+          options={filterOpt.media.choices}
+          size="sm"
+          onChange={onChangeSelectMedia}
+        />
+      </Box>
+      <Box>
+        <SelectWithLabel
+          labelText="Media"
+          options={filterOpt.terms.choices}
+          size="sm"
+          onChange={onChangeSelectTerms}
+        />
+      </Box>
+      <Box>
+        <SelectWithLabel
+          labelText="Terms"
+          options={filterOpt.language.choices}
+          size="sm"
+          onChange={onChangeSelectLanguage}
+        />
+      </Box>
+    </Flex>
+  );
+});
