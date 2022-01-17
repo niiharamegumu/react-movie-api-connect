65e18e6e0ce53bf7d462ced3bf81ddffbea797bf
Megumu Niihara <nhrmgm1223@gmail.com>
2022-01-17 16:04:52 +0900

    update--画面幅を取得するhooks追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import "@fontsource/noto-sans-georgian";
 
 import { MvList } from "./component/organisms/MvList";
 import { SearchMv } from "./component/organisms/SearchMv";
+import { useWindowDimensions } from "./hooks/useWindowDimensions";
 import theme from "./theme/theme";
 
 function App() {
+  const { width } = useWindowDimensions();
   return (
     <ChakraProvider theme={theme}>
       <Box p="30px 0">
         <SearchMv />
-        <MvList title="Global Trending Now." isLargeRow={false} />
+        <MvList
+          title="Global Trending Now."
+          isLargeRow={width > 740 ? true : false}
+        />
       </Box>
     </ChakraProvider>
   );
