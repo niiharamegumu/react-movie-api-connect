50e27481a60d9e93565af433b97d4ddbcd29537d
Megumu Niihara <nhrmgm1223@gmail.com>
2022-01-17 16:05:30 +0900

    add--modal component追加

---
+++ src/component/organisms/SelectMvModal.tsx
@@ -0,0 +1,65 @@
+import {
+  Badge,
+  Text,
+  Button,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Box,
+  Image,
+} from "@chakra-ui/react";
+import { VFC } from "react";
+
+import { ImageBaseUrl } from "../../config/mv-api/settings";
+import { Movie } from "../../type/movie";
+
+type Props = {
+  isOpen: boolean;
+  movie: Movie;
+  onClose: () => void;
+};
+
+export const SelectMvModal: VFC<Props> = (props) => {
+  const { isOpen, movie, onClose } = props;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size="3xl" isCentered={true}>
+      <ModalContent
+        borderRadius={0}
+        backdropFilter="blur(14px)"
+        bgColor="rgba(0,0,0,.5)"
+      >
+        <Box>
+          <Image src={`${ImageBaseUrl}${movie.backdrop_path}`} />
+        </Box>
+        <ModalHeader>
+          <Heading as="h2" fontSize={{ base: "sm", sm: "lg" }}>
+            {movie.title || movie.name}
+          </Heading>
+          <Text fontSize="sm">
+            {movie.original_title || movie.original_name}
+          </Text>
+          <Badge colorScheme="teal">{movie.media_type}</Badge>
+          <Text fontSize="sm" mb={2}>
+            Release : {movie.release_date || "Not Data ..."}
+          </Text>
+        </ModalHeader>
+        <ModalBody>{movie.overview || "Not Description ..."}</ModalBody>
+
+        <ModalFooter>
+          <Button
+            colorScheme="blackAlpha"
+            mr={3}
+            onClick={onClose}
+            bgColor="rgba(0,0,0,0)"
+            _hover={{ bgColor: "rgba(255,255,255)", color: "black" }}
+          >
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
