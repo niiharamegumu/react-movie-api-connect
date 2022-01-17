7eac32d26354b68a92de413e862b690c126595f0
Megumu Niihara <nhrmgm1223@gmail.com>
2022-01-17 16:06:33 +0900

    update--movie type追加

---
+++ src/type/movie.ts
@@ -0,0 +1,13 @@
+export type Movie = {
+  id: string;
+  title?: string;
+  name?: string;
+  original_title?: string;
+  original_name?: string;
+  poster_path: string;
+  backdrop_path: string;
+  overview: string;
+  release_date: string;
+  media_type: string;
+  vote_average: number;
+};
