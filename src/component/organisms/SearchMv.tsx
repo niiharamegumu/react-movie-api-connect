a97c2c98e00d440ff8e7933555ed078533d9e8ee
Megumu Niihara <nhrmgm1223@gmail.com>
2022-01-24 14:20:16 +0900

    update--検索Fieldの出し分け機能追加

--- src/component/organisms/SearchMv.tsx
+++ src/component/organisms/SearchMv.tsx
@@ -1,23 +1,50 @@
 import { Box, Flex } from "@chakra-ui/react";
-import { useState, ChangeEvent, VFC, memo, useEffect } from "react";
+import {
+  useState,
+  ChangeEvent,
+  VFC,
+  memo,
+  useEffect,
+  useCallback,
+} from "react";
 
 import { filterOpt } from "../../config/search/settings";
 import { useSearchFilter } from "../../hooks/useSearchFilter";
 import { SelectWithLabel } from "../molecules/input/SelectWithLabel";
 
-export const SearchMv: VFC = memo(() => {
+type Props = {
+  filters: string[];
+};
+
+export const SearchMv: VFC<Props> = memo((props) => {
+  const { filters } = props;
+
   const { searchFilter, setSearchFilter } = useSearchFilter();
   const [selectLanguage, setSelectLanguage] = useState(searchFilter.language);
   const [selectMedia, setSelectMedia] = useState(searchFilter.media);
   const [selectTerms, setSelectTerms] = useState(searchFilter.terms);
+  const [selectMvGenreId, setSelectMvGenreId] = useState(
+    searchFilter.mvGenreId
+  );
+  const [selectTvGenreId, setSelectTvGenreId] = useState(
+    searchFilter.tvGenreId
+  );
 
   useEffect(() => {
     setSearchFilter({
       language: selectLanguage,
       media: selectMedia,
       terms: selectTerms,
+      mvGenreId: selectMvGenreId,
+      tvGenreId: selectTvGenreId,
     });
-  }, [selectLanguage, selectMedia, selectTerms]);
+  }, [
+    selectLanguage,
+    selectMedia,
+    selectTerms,
+    selectMvGenreId,
+    selectTvGenreId,
+  ]);
 
   const onChangeSelectLanguage = (e: ChangeEvent<HTMLSelectElement>) =>
     setSelectLanguage(e.target.value);
@@ -25,6 +52,17 @@ export const SearchMv: VFC = memo(() => {
     setSelectMedia(e.target.value);
   const onChangeSelectTerms = (e: ChangeEvent<HTMLSelectElement>) =>
     setSelectTerms(e.target.value);
+  const onChangeSelectMvGenreId = (e: ChangeEvent<HTMLSelectElement>) =>
+    setSelectMvGenreId(e.target.value);
+  const onChangeSelectTvGenreId = (e: ChangeEvent<HTMLSelectElement>) =>
+    setSelectTvGenreId(e.target.value);
+
+  const isExistence = useCallback(
+    (filter: string) => {
+      return filters.find((item) => item === filter);
+    },
+    [filters]
+  );
 
   return (
     <Flex
@@ -36,7 +74,7 @@ export const SearchMv: VFC = memo(() => {
       mb={10}
       p="0 20px"
     >
-      <Box>
+      <Box display={isExistence("Media") ? "block" : "none"}>
         <SelectWithLabel
           labelText="Media"
           options={filterOpt.media.choices}
@@ -44,7 +82,7 @@ export const SearchMv: VFC = memo(() => {
           onChange={onChangeSelectMedia}
         />
       </Box>
-      <Box>
+      <Box display={isExistence("Terms") ? "block" : "none"}>
         <SelectWithLabel
           labelText="Terms"
           options={filterOpt.terms.choices}
@@ -52,12 +90,28 @@ export const SearchMv: VFC = memo(() => {
           onChange={onChangeSelectTerms}
         />
       </Box>
-      <Box>
+      <Box display={isExistence("Language") ? "block" : "none"}>
         <SelectWithLabel
           labelText="Language"
           options={filterOpt.language.choices}
           size="sm"
           onChange={onChangeSelectLanguage}
+        />
+      </Box>
+      <Box display={isExistence("mvGenre") ? "block" : "none"}>
+        <SelectWithLabel
+          labelText="Genre"
+          options={filterOpt.mvGenres.choices}
+          size="sm"
+          onChange={onChangeSelectMvGenreId}
+        />
+      </Box>
+      <Box display={isExistence("tvGenre") ? "block" : "none"}>
+        <SelectWithLabel
+          labelText="Genre"
+          options={filterOpt.tvGenres.choices}
+          size="sm"
+          onChange={onChangeSelectTvGenreId}
         />
       </Box>
     </Flex>
